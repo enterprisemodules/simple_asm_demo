16063cc6969cf6a0eb0f04ae454685054e782d4a
Remy van Berkum <remy.van.berkum@enterprisemodules.com>
2022-11-07 17:24:52 +0100

    Fix setup_puppet.sh
    Change-Id: Ibfa3d6d7494547f3c4c9acb22b2c50e5d7bd09ca

--- vm-scripts/setup_puppet.sh
+++ vm-scripts/setup_puppet.sh
@@ -6,7 +6,7 @@ else
   # Install R10K. We need this to download the correct set of puppet modules
   #
   echo 'Installing required gems'
-  /opt/puppetlabs/puppet/bin/gem install r10k --no-rdoc --no-ri > /dev/null # 2>&1
+  /opt/puppetlabs/puppet/bin/gem install r10k --no-document > /dev/null # 2>&1
 
   echo 'Installing required puppet modules'
   cd /vagrant
@@ -17,7 +17,19 @@ else
   then
     cp /vagrant/.netrc ~
   fi
-  /opt/puppetlabs/puppet/bin/r10k puppetfile install --force > /dev/null # 2>&1
+  #
+  # Check which Puppetfile to use. If a file use_latest exists, use the
+  # Puppetfile_latest which by default fetches files from latest master branch
+  #
+  if [ -e /vagrant/use_latest_modules ]
+  then
+    echo "Using latest versions of modules..."
+    PUPPETFILE="Puppetfile_latest"
+  else
+    echo "Using released versions of modules..."
+    PUPPETFILE="Puppetfile"
+  fi
+  /opt/puppetlabs/puppet/bin/r10k puppetfile install --puppetfile ${PUPPETFILE} --force > /dev/null # 2>&1
 
   #
   # Setup hiera search and backend. We need this to config our systems
